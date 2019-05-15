c4e39514b3ef35988850dbef8d51926b0b71a3ce
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-05-15 13:44:52 +0200

    update to project structure

--- src/LatPhysBandstructuresPlottingPyPlot.jl
+++ src/LatPhysBandstructuresPlottingPyPlot.jl
@@ -1,5 +1,36 @@
+################################################################################
+#
+#   module LatPhysBandstructuresPlottingPyPlot
+#   -> PyPlot
+#   -> LatPhysBase
+#   -> LatPhysReciprocal
+#   -> LatPhysBandstructures
+#   -> LinearAlgebra
+#
+#   --> PLOTTING OF BANDSTRUCTURES AND FERMI SURFACES
+#           - 2D lattices
+#           - 3D lattices
+#
+################################################################################
+
+
+# start of module
 module LatPhysBandstructuresPlottingPyPlot
 
-greet() = print("Hello World!")
+# used libraries
+using LatPhysBase
+using LatPhysReciprocal
+using LatPhysBandstructures
+using LinearAlgebra
+using LatPhysPlottingPyPlot
+using LaTeXStrings
+using PyPlot
+
+# explicitly import PyPlot.plot to overwrite
+import PyPlot.plot
+
+
+
+
 
 end # module
